f0cfe4bc296fabc96803565621a7651bb4099c1d
Dhruvin Beladiya <dhruvinbeladiya01@gmail.com>
2023-03-06 01:19:10 +0530

    completed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,261 @@
 $(document).ready(() => {
-
+    
     // signup login page hide show with Jquery
     $(".loginform").hide();
+    $(".loginicon").hide();
 
     $("#signuplink").click(() => {
-        $(".signupform").show();
+        $(".signupform").slideDown(500);
         $(".loginform").hide();
         $("#loginlink").removeClass("active");
         $("#signuplink").addClass("active");
+        $(".signupicon").slideDown(300);
+        $(".loginicon").hide();
     });
-    
+
     $("#loginlink").click(() => {
-        $(".loginform").show();
+        $(".loginform").slideDown(500);
         $(".signupform").hide();
         $("#signuplink").removeClass("active");
         $("#loginlink").addClass("active");
+        $(".loginicon").slideDown(300);
+        $(".signupicon").hide();
     });
+
+    //Signup form Validation with Jquery
+    var nameerr = true;
+    var emailerr = true;
+    var passerr = true;
+    var confirmerr = true;
+    var loginemailerr = true;
+    var loginpasserr = true;
+    $("#name").keyup(function () {
+        namecheck();
+    });
+
+    $("#email").keyup(function () {
+        emailcheck();
+    })
+
+    $("#pass").keyup(function () {
+        passcheck();
+    })
+
+    $("#confirmpass").keyup(function () {
+        confirmpasscheck();
+    })
+
+    function namecheck() {
+        var name = $("#name").val();
+        var nametext = $("#nametext");
+
+        if (name == '') {
+            nametext.text("*Please Fill Name");
+            nameerr = false;
+            return false;
+        }
+        else {
+            nametext.text("");
+        }
+
+        if (!/^[A-Za-z ]+$/.test(name)) {
+            nametext.text("*Name contains only alphabets");
+            nameerr = false;
+            return false;
+        }
+        else {
+            nametext.text("");
+        }
+
+        if (name.length < 3 || name.length > 10) {
+            nametext.text("*Name must be between 3 and 10");
+            nameerr = false;
+            return false;
+        }
+        else {
+            nametext.text("");
+        }
+    }
+
+    function emailcheck() {
+        var email = $("#email").val();
+        var emailtext = $("#emailtext");
+        var emailregex = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+
+        if (email == '') {
+            emailtext.text("*Please Fill Email");
+            emailerr = false;
+            return false;
+        }
+        else {
+            emailtext.text("");
+        }
+
+        if (!emailregex.test(email)) {
+            emailtext.text("*Enter Valid Email");
+            emailerr = false;
+            return false;
+        }
+        else {
+            emailtext.text("");
+        }
+    }
+
+    function passcheck() {
+        let pass = $("#pass").val();
+        let passtext = $("#passtext");
+        let passregex = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+
+        if (pass == '') {
+            passtext.text("*Please Fill Password");
+            passerr = false;
+            return false;
+        }
+        else {
+            passtext.text("");
+        }
+
+        if (pass.length < 6) {
+            passtext.text("*Password must be greater than 6 length");
+            passerr = false;
+            return false;
+        }
+        else {
+            passtext.text("");
+        }
+
+        if (!passregex.test(pass)) {
+            passtext.text("*Pass must contain at least 1 special character");
+            passerr = false;
+            return false;
+        }
+        else {
+            passtext.text("");
+        }
+    }
+
+    function confirmpasscheck() {
+        let pass = $("#pass").val();
+        let confirmpass = $("#confirmpass").val();
+        let confirmtext = $("#confirmtext");
+
+        if (confirmpass == '') {
+            confirmtext.text("*Please Confirm Password Again");
+            confirmerr = false;
+            return false;
+        }
+        else {
+            confirmtext.text("");
+        }
+
+        if (confirmpass != pass) {
+            confirmtext.text("*Confirm pass doesn' match the pass");
+            confirmerr = false;
+            return false;
+        }
+        else {
+            confirmtext.text("");
+        }
+    }
+
+    $("#signupbtn").click(function () {   
+        nameerr = true;
+        emailerr = true;
+        passerr = true;
+        confirmerr = true;
+
+        namecheck();
+        emailcheck();
+        passcheck();
+        confirmpasscheck();
+
+        if((nameerr==true) && (emailerr==true) && (passerr==true) && (confirmerr==true)) {
+            swal('Good job!', 'You Have Been Registered Succesfully', 'success');
+            document.getElementById("resetsignup").reset();
+            return true;
+        } 
+        else {
+            return false;
+        }
+    })
+
+    //Login form validation with Jquery
+
+    $("#loginemail").keyup(function () {
+        loginemailcheck();
+    });
+
+    $("#loginpass").keyup(function () {
+        loginpasscheck();
+    });
+
+    function loginemailcheck() {
+        var loginemail = $("#loginemail").val();
+        var loginemailtxt = $("#loginemailtxt");
+        var loginemailregex = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+
+        if (loginemail == '') {
+            loginemailtxt.text("*Email Required to login");
+            loginemailerr =false;
+            return false;
+        }
+        else {
+            loginemailtxt.text("");
+        }
+
+        if (!loginemailregex.test(loginemail)) {
+            loginemailtxt.text("*Enter Valid Email");
+            loginemailerr=false;
+            return false;
+        }
+        else {
+            loginemailtxt.text("");
+        }
+    }
+
+    function loginpasscheck() {
+        let loginpass = $("#loginpass").val();
+        let loginpasstext = $("#loginpasstxt");
+        let loginpassregex = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+
+        if (loginpass == '') {
+            loginpasstext.text("*Enter Password to login");
+            loginpasserr = false;
+            return false;
+        }
+        else {
+            loginpasstext.text("");
+        }
+
+        if ((loginpass.length < 6) || (!loginpassregex.test(loginpass))) {
+            loginpasstext.text("*Your Password doen't match");
+            loginpasserr = false;
+            return false;
+        }
+        else {
+            loginpasstext.text("");
+        }
+    }
+
+    $("#loginbtn").click(function () {   
+        loginemailerr = true;
+        loginpasserr = true;
+
+        loginemailcheck();
+        loginpasscheck();
+
+        if((loginemailerr==true) && (loginpasserr==true) ) {
+            swal('Welcome!', 'Your Login Succesfully', 'success');
+            document.getElementById("resetlogin").reset();
+        } 
+        else {
+            return false;
+        }
+    })
 
     // Footer Append In Every page with Jquery
     $('footer').append(
-    `
+        `
         <div class="suscribe py-5">
             <div class="container">
                 <div class="row justify-content-center">
@@ -42,13 +278,12 @@ $(document).ready(() => {
                 <div class="row justify-content-between">
                     <div class="col-md-3 mb-md-0 mb-5">
                         <div class="footer_info">
-                            <h5 class="text-white mb-4">Information</h5> 
-                            <h6 class="mb-1">Address -</h6>
-                            <p class="mb-3">141 , Apple Square , Anand mahal road , adajan , surat-394211</p> 
-                            <h6 class="mb-1">Contact -</h6>
-                            <p class="mb-3">+91 63457 12392</p>
-                            <h6 class="mb-1">Email -</h6>
-                            <p>info@dhruvoelectronics.com</p>
+                            <div class="footer_logo mb-3">
+                                <img src="./images/logo-white.png" width="200px"></img>
+                            </div>
+                            <p class="pt-2">141 , Apple Square , Anand mahal road , adajan , surat-394211</p> 
+                            <p class="pt-3">+91 63457 12392</p>
+                            <p class="pt-3">info@dhruvoelectronics.com</p>
                         </div>
                     </div>
                     <div class="col-md-2 mb-md-0 mb-5">
@@ -155,11 +390,11 @@ const CountDown = () => {
     let CurrentDate = new Date();
     let FutureDate = new Date("6 April 2023");
     let myDate = FutureDate - CurrentDate;
-    
+
     let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
-    let hrs = Math.floor(myDate / 1000 / 60 / 60) %24;
-    let mins = Math.floor(myDate / 1000 / 60 ) % 60;
-    let secs = Math.floor(myDate / 1000 ) % 60;
+    let hrs = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let mins = Math.floor(myDate / 1000 / 60) % 60;
+    let secs = Math.floor(myDate / 1000) % 60;
 
     dayItem.innerHTML = days;
     hrsItem.innerHTML = hrs;
@@ -168,7 +403,7 @@ const CountDown = () => {
 }
 
 CountDown();
-setInterval(CountDown,1000);
+setInterval(CountDown, 1000);
 
 
 
